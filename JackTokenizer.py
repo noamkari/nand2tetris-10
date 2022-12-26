b6d5a36038580978e71f699ec91543286bd1368b
noam <noam.kari@huji.com>
2022-12-26 12:43:09 +0200

    pass some tests

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -8,13 +8,12 @@ Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
 import re
 import typing
 
-KEYWORDS = ['class', 'constructor', 'function', 'method', 'field',
-            'static', 'var', 'int', 'char', 'boolean', 'void', 'true',
-            'false', 'null', 'this', 'let', 'do', 'if', 'else',
-            'while', 'return']
-
-SYMBOLS = {'{', '}', '(', ')', '[', ']', '.', ',', ';', '+',
-           '-', '*', '/', '&', ',', '<', '>', '=', '~', '^', '#'}
+KEYORDS = ['class', 'constructor', 'function', 'method', 'field',
+           'static', 'var', 'int', 'char', 'boolean', 'void', 'true',
+           'false', 'null', 'this', 'let', 'do', 'if', 'else',
+           'while', 'return']
+SYMBOLS = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+',
+           '-', '*', '/', '&', ',', '<', '>', '=', '~', '^', '#']
 
 
 def find_quoted_substrings(s: str):
@@ -58,7 +57,9 @@ def remove_comments(jack_file):
 
     # Replace all matches with an empty string
     jack_file = re.sub(regex, "", jack_file)
-    out_list = jack_file.split()
+    # out_list = jack_file.split()
+    out_list = re.findall(
+        r'[\w]+|[*\{\}()\[\].,;+\\\-&/|<>=~\?]|[\"\'].+[\"\']', jack_file)
 
     for s in out_list:
         if s in dct:
@@ -67,43 +68,21 @@ def remove_comments(jack_file):
     return out_list
 
 
-def helper(out_list, find):
-
-    if not find:
-        return out_list
-
-    find = False
-    cut_strings = []
-    for string in out_list:
-        for i, char in enumerate(string):
-            if char in SYMBOLS:
-                if i != 0:
-                    cut_strings.append(string[:i])
-                if i != len(string):
-                    cut_strings.append(string[i:])
-                find = True
-                break
-        else:
-            cut_strings.append(string)
-
-    return helper(out_list, find)
-
-
 class JackTokenizer:
     """Removes all comments from the input stream and breaks it
     into Jack language tokens, as specified by the Jack grammar.
-
+    
     # Jack Language Grammar
 
     A Jack file is a stream of characters. If the file represents a
     valid program, it can be tokenized into a stream of valid tokens. The
-    tokens may be separated by an arbitrary number of whitespace characters,
-    and comments, which are ignored. There are three possible comment formats:
-    /* comment until closing */ , /** API comment until closing */ , and
+    tokens may be separated by an arbitrary number of whitespace characters, 
+    and comments, which are ignored. There are three possible comment formats: 
+    /* comment until closing */ , /** API comment until closing */ , and 
     // comment until the line’s end.
 
     - ‘xxx’: quotes are used for tokens that appear verbatim (‘terminals’).
-    - xxx: regular typeface is used for names of language constructs
+    - xxx: regular typeface is used for names of language constructs 
            (‘non-terminals’).
     - (): parentheses are used for grouping of language constructs.
     - x | y: indicates that either x or y can appear.
@@ -114,29 +93,29 @@ class JackTokenizer:
 
     The Jack language includes five types of terminal elements (tokens).
 
-    - keyword: 'class' | 'constructor' | 'function' | 'method' | 'field' |
+    - keyword: 'class' | 'constructor' | 'function' | 'method' | 'field' | 
                'static' | 'var' | 'int' | 'char' | 'boolean' | 'void' | 'true' |
-               'false' | 'null' | 'this' | 'let' | 'do' | 'if' | 'else' |
+               'false' | 'null' | 'this' | 'let' | 'do' | 'if' | 'else' | 
                'while' | 'return'
-    - symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' |
+    - symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' | 
               '-' | '*' | '/' | '&' | '|' | '<' | '>' | '=' | '~' | '^' | '#'
     - integerConstant: A decimal number in the range 0-32767.
-    - StringConstant: '"' A sequence of Unicode characters not including
+    - StringConstant: '"' A sequence of Unicode characters not including 
                       double quote or newline '"'
-    - identifier: A sequence of letters, digits, and underscore ('_') not
+    - identifier: A sequence of letters, digits, and underscore ('_') not 
                   starting with a digit. You can assume keywords cannot be
                   identifiers, so 'self' cannot be an identifier, etc'.
 
     ## Program Structure
 
-    A Jack program is a collection of classes, each appearing in a separate
-    file. A compilation unit is a single class. A class is a sequence of tokens
+    A Jack program is a collection of classes, each appearing in a separate 
+    file. A compilation unit is a single class. A class is a sequence of tokens 
     structured according to the following context free syntax:
-
+    
     - class: 'class' className '{' classVarDec* subroutineDec* '}'
     - classVarDec: ('static' | 'field') type varName (',' varName)* ';'
     - type: 'int' | 'char' | 'boolean' | className
-    - subroutineDec: ('constructor' | 'function' | 'method') ('void' | type)
+    - subroutineDec: ('constructor' | 'function' | 'method') ('void' | type) 
     - subroutineName '(' parameterList ')' subroutineBody
     - parameterList: ((type varName) (',' type varName)*)?
     - subroutineBody: '{' varDec* statements '}'
@@ -148,28 +127,28 @@ class JackTokenizer:
     ## Statements
 
     - statements: statement*
-    - statement: letStatement | ifStatement | whileStatement | doStatement |
+    - statement: letStatement | ifStatement | whileStatement | doStatement | 
                  returnStatement
     - letStatement: 'let' varName ('[' expression ']')? '=' expression ';'
-    - ifStatement: 'if' '(' expression ')' '{' statements '}' ('else' '{'
+    - ifStatement: 'if' '(' expression ')' '{' statements '}' ('else' '{' 
                    statements '}')?
     - whileStatement: 'while' '(' 'expression' ')' '{' statements '}'
     - doStatement: 'do' subroutineCall ';'
     - returnStatement: 'return' expression? ';'
 
     ## Expressions
-
+    
     - expression: term (op term)*
-    - term: integerConstant | stringConstant | keywordConstant | varName |
-            varName '['expression']' | subroutineCall | '(' expression ')' |
+    - term: integerConstant | stringConstant | keywordConstant | varName | 
+            varName '['expression']' | subroutineCall | '(' expression ')' | 
             unaryOp term
-    - subroutineCall: subroutineName '(' expressionList ')' | (className |
+    - subroutineCall: subroutineName '(' expressionList ')' | (className | 
                       varName) '.' subroutineName '(' expressionList ')'
     - expressionList: (expression (',' expression)* )?
     - op: '+' | '-' | '*' | '/' | '&' | '|' | '<' | '>' | '='
     - unaryOp: '-' | '~' | '^' | '#'
     - keywordConstant: 'true' | 'false' | 'null' | 'this'
-
+    
     Note that ^, # correspond to shiftleft and shiftright, respectively.
     """
 
@@ -200,8 +179,8 @@ class JackTokenizer:
         return self.cur < len(self._input_lines)
 
     def advance(self) -> None:
-        """Gets the next token from the input and makes it the current token.
-        This method should be called if has_more_tokens() is true.
+        """Gets the next token from the input and makes it the current token. 
+        This method should be called if has_more_tokens() is true. 
         Initially there is no current token.
         """
         # Your code goes here!
@@ -215,7 +194,7 @@ class JackTokenizer:
         """
         # Your code goes here!
         cur_token = self._input_lines[self.cur]
-        if cur_token in KEYWORDS:
+        if cur_token in KEYORDS:
             return "KEYWORD"
         elif cur_token in SYMBOLS:
             return "SYMBOL"
@@ -232,12 +211,12 @@ class JackTokenizer:
         Returns:
             str: the keyword which is the current token.
             Should be called only when token_type() is "KEYWORD".
-            Can return "CLASS", "METHOD", "FUNCTION", "CONSTRUCTOR", "INT",
-            "BOOLEAN", "CHAR", "VOID", "VAR", "STATIC", "FIELD", "LET", "DO",
+            Can return "CLASS", "METHOD", "FUNCTION", "CONSTRUCTOR", "INT", 
+            "BOOLEAN", "CHAR", "VOID", "VAR", "STATIC", "FIELD", "LET", "DO", 
             "IF", "ELSE", "WHILE", "RETURN", "TRUE", "FALSE", "NULL", "THIS"
         """
         # Your code goes here!
-        return self._input_lines[self.cur]
+        return self._input_lines[self.cur].upper()
 
     def symbol(self) -> str:
         """
@@ -245,7 +224,7 @@ class JackTokenizer:
             str: the character which is the current token.
             Should be called only when token_type() is "SYMBOL".
             Recall that symbol was defined in the grammar like so:
-            symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' |
+            symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' | 
               '-' | '*' | '/' | '&' | '|' | '<' | '>' | '=' | '~' | '^' | '#'
         """
         # Your code goes here!
@@ -257,7 +236,7 @@ class JackTokenizer:
             str: the identifier which is the current token.
             Should be called only when token_type() is "IDENTIFIER".
             Recall that identifiers were defined in the grammar like so:
-            identifier: A sequence of letters, digits, and underscore ('_') not
+            identifier: A sequence of letters, digits, and underscore ('_') not 
                   starting with a digit. You can assume keywords cannot be
                   identifiers, so 'self' cannot be an identifier, etc'.
         """
@@ -278,10 +257,10 @@ class JackTokenizer:
     def string_val(self) -> str:
         """
         Returns:
-            str: the string value of the current token, without the double
+            str: the string value of the current token, without the double 
             quotes. Should be called only when token_type() is "STRING_CONST".
             Recall that StringConstant was defined in the grammar like so:
-            StringConstant: '"' A sequence of Unicode characters not including
+            StringConstant: '"' A sequence of Unicode characters not including 
                       double quote or newline '"'
         """
         # Your code goes here!
@@ -289,7 +268,7 @@ class JackTokenizer:
 
     def cur_token(self):
         if self.token_type() == "KEYWORD":
-            return self.keyword()
+            return self.keyword().lower()
         if self.token_type() == "SYMBOL":
             return self.symbol()
         if self.token_type() == "IDENTIFIER":
